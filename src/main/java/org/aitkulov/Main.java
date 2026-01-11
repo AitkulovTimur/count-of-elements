079f830d1587c94d87a742c3dba821933900ae90
timuraitkulov <taitkulov@phoenixit.ru>
2026-01-11 14:20:00 +0500

    feat[resolution]: implement function that takes an array and return occurrences map

--- src/main/java/org/aitkulov/Main.java
+++ src/main/java/org/aitkulov/Main.java
@@ -1,7 +1,26 @@
 package org.aitkulov;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        //test
+        Map<Integer, Integer> result = countOccurrencesOfElements(new Integer[] {1, 1, 1, 1, 1, 6, 7, 8, 9, 10});
+        for (Map.Entry<Integer, Integer> entry : result.entrySet()) {
+            System.out.println("Key:" + entry.getKey() + " Occurrences:" + entry.getValue());
+        }
+    }
+
+    public static <T> Map<T, Integer> countOccurrencesOfElements(T[] array) {
+        Map<T, Integer> resultMap = new HashMap<>();
+
+        for (T obj : array) {
+            resultMap.put(obj, resultMap.getOrDefault(obj, 0) + 1);
+        }
+
+        return resultMap;
     }
 }
