83f517550fdc68613c694ddfea4c6549aba87063
timuraitkulov <taitkulov@phoenixit.ru>
2026-01-11 14:29:51 +0500

    feat[test]: implement test

--- src/main/java/org/aitkulov/Main.java
+++ src/main/java/org/aitkulov/Main.java
@@ -5,13 +5,22 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("test Integer");
 
-        //test
+        //test Integer
         Map<Integer, Integer> result = countOccurrencesOfElements(new Integer[] {1, 1, 1, 1, 1, 6, 7, 8, 9, 10});
         for (Map.Entry<Integer, Integer> entry : result.entrySet()) {
             System.out.println("Key:" + entry.getKey() + " Occurrences:" + entry.getValue());
         }
+
+        System.out.println("test String");
+
+        //test String
+        Map<String, Integer> result2 = countOccurrencesOfElements(new String[] {"a", "b", "c", "a", "b", "c", "a", "b", "c", "a"});
+        for (Map.Entry<String, Integer> entry : result2.entrySet()) {
+            System.out.println("Key:" + entry.getKey() + " Occurrences:" + entry.getValue());
+        }
+
     }
 
     public static <T> Map<T, Integer> countOccurrencesOfElements(T[] array) {
